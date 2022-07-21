8bc9bcea73f488f1cd25fe3982d7810ff9af4e93
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-07-20 21:35:50 -0300

    funcao de sortear e exibir cores e invocada dentro do eveto global onload

--- index.js
+++ index.js
@@ -4,36 +4,40 @@ const secondBox = document.getElementById('second-box');
 const thirdBox = document.getElementById('third-box');
 const fourthBox = document.getElementById('fourth-box');
 const fifthBox = document.getElementById('fifth-box');
-const sixthBox = document.getElementById('sixth-box');
+const boxToHit = document.getElementById('sixth-box');
 
 let par1, par2, par3;
 
+window.onload = () => {
+    drawColors();
+};
+
 function drawRGB() {
-    let rgbPar = [par1, par2, par3];
+    let rgbArray = [par1, par2, par3];
 
     for (let i = 0; i < 3; i++) {
-        rgbPar[i] = Math.floor(Math.random() * 256)
+        rgbArray[i] = Math.floor(Math.random() * 256)
     }
 
-    let valuesFormated = rgbPar.join(',')
+    TitleColor(rgbArray);
 
-    return(valuesFormated)
+    let valuesFormated = rgbArray.join(',');
+
+    return valuesFormated;
 }
 
-function rgbTitle(parameter) {
-    title.innerText = `RGB(${parameter[0]}, ${parameter[1]}, ${parameter[2]})`
+function TitleColor(parameter) {
+    title.innerText = `RGB(${parameter[0]}, ${parameter[1]}, ${parameter[2]})`; 
+
 }
 
 function drawColors() {
     firstBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
     secondBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
     thirdBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
-    thirdBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
     fourthBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
     fifthBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
-    sixthBox.style.backgroundColor = "rgb(" + drawRGB() + ")";
+    boxToHit.style.backgroundColor = "rgb(" + drawRGB() + ")";
 }
 
 
-drawRGB()
-drawColors()

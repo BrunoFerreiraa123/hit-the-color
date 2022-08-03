fc931a9809564c035dbe2fe0879b04a34adf6dee
Bruno Ferreira <bruno55ferreira55@gmail.com>
2022-08-03 17:50:21 -0300

    as constante que fazem referencia aos cards HTML agora ficam nesse modulo e sao exportas para os arquivos em que sao necessarias

---
+++ modules/cardsArray.js
@@ -0,0 +1,10 @@
+const firstBox = document.getElementById('first-box');
+const secondBox = document.getElementById('second-box');
+const thirdBox = document.getElementById('third-box');
+const fourthBox = document.getElementById('fourth-box');
+const fifthBox = document.getElementById('fifth-box');
+const sixthBox = document.getElementById('sixth-box');
+
+const arrCards = [firstBox, secondBox, thirdBox, fourthBox, fifthBox, sixthBox];
+
+export default arrCards;
